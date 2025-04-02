b912ac1438d9615b6712c9c2a176184d7e2b98d0
Lucas Fadini Apolinario <leandro.lucasrato2013@gmail.com>
2025-04-02 11:34:19 -0300

    Base do conversor de moedas concluido. Programa Funcional!

--- Curso_Python/Conversor de Moedas/main.py
+++ Curso_Python/Conversor de Moedas/main.py
@@ -1,12 +1,13 @@
 import customtkinter
 from pegar_moedas import nomes_moedas, conversoes_disponiveis
+from receber_cotacoes import converter_moeda
 
 #Congfig janela principal
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 janela = customtkinter.CTk()
-janela.geometry("500x500")
+janela.geometry("500x600")
 
 dic_conversoes_disponiveis = conversoes_disponiveis()
 
@@ -24,10 +25,17 @@ campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=list(dic_convers
 campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values=["Selecione uma Moeda de Origem"])
 
 #Ação do programa
-def converter ():
-    print("Convertido")
+def converter():
+    moeda_origem = campo_moeda_origem.get()
+    moeda_destino = campo_moeda_destino.get()
+    if moeda_origem and moeda_destino:
+        cotacao = converter_moeda(moeda_origem, moeda_destino)
+        texto_cotacao_moeda.configure(text=f"Valor atualizado da cotação:\n1 {moeda_origem} = {cotacao} {moeda_destino}")
 
 botao = customtkinter.CTkButton(janela, text="Converter", command=converter)
+
+#Texto da cotação final (Sem config)
+texto_cotacao_moeda = customtkinter.CTkLabel(janela, text="")
 
 #Janela Scrolavel de Moedas
 lista_moedas = customtkinter.CTkScrollableFrame(janela)
@@ -45,7 +53,8 @@ texto_moeda_origem.pack(padx=10, pady=10)
 campo_moeda_origem.pack(padx=10)
 texto_moeda_destino.pack(padx=10, pady=10)
 campo_moeda_destino.pack(padx=10)
-botao.pack(padx=10, pady=50)
+botao.pack(padx=10, pady=35)
+texto_cotacao_moeda.pack(padx=10, pady=10)
 lista_moedas.pack(padx=10)
 
 #mainloop
