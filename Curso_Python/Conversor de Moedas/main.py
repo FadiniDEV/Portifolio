2466624173523edd66bbcb9d7e952c8f429259f0
Lucas Fadini <leandro.lucasrato2013@gmail.com>
2025-07-18 12:21:11 -0300

    Comitando

--- Curso_Python/Conversor de Moedas/main.py
+++ Curso_Python/Conversor de Moedas/main.py
@@ -7,7 +7,7 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 janela = customtkinter.CTk()
-janela.geometry("500x600")
+janela.geometry("500x650")
 
 dic_conversoes_disponiveis = conversoes_disponiveis()
 
